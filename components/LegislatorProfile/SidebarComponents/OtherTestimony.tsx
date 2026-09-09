0685c46653713cad027c638e21d8a377e4353a8c
mertbagt <mertbagt@gmail.com>
2026-09-08 22:02:23 -0400

    revert limit

--- components/LegislatorProfile/SidebarComponents/OtherTestimony.tsx
+++ components/LegislatorProfile/SidebarComponents/OtherTestimony.tsx
@@ -104,7 +104,7 @@ export const OtherTestimony = ({
 }) => {
   const { t } = useTranslation("legislators")
 
-  const randomBills: any[] = shuffleArray(sponsoredBills).slice(0, 10)
+  const randomBills: any[] = shuffleArray(sponsoredBills)
 
   return (
     <SidebarBlock className="mb-2">
